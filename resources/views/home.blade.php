a15cf5913843e0f5d7f20dd03301fbe9a64e170b
CharizardMissay <735500413@qq.com>
2017-06-01 22:47:09 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,7 +82,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    
             
                 </div>
             </div>
